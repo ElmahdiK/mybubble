e44048e96aae20d86826ac2705dabb76e0d6fc6a
ElmahdiK <elmahdi.korfed@gmail.com>
2024-12-05 22:17:17 +0100

    fix(json): mise à jour de quelques liens

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,31 +6,35 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 const _pathJSON = `./assets/data/mybubble.json`;
-let _data;
 
 window.onload = _ => {
     console.log(`page loaded`);
 
     let _article = $(`article`);
-    let _dates = [];
 
     _loadData().then(d => {
         let _html = ``;
         // _html += `<div>`;
         for (let i = 0, j = d.length; i < j; i++) {
-            _html += `<details open><summary>${d[i].type}</summary><div>`;
+            // _html += `<details open><summary>${d[i].type}</summary><div>`;
             for (let a = 0, b = d[i].content.length; a < b; a++) _html += content(d[i].content[a]);
-            _html += `</div></details>`;
+            // _html += `</div></details>`;
         }
         // _html += `</div>`;
         _article.insertAdjacentHTML(`beforeEnd`, _html);
     });
+
+    $(`#bt-modal`).onclick=()=>{
+        seeContent(null);
+        closeModal();
+    }
 }
 const content = _content => { return `<img src="${_content.img}" data-title="${_content.title}" data-video="${_content.video}" onclick='seeContent(this)'>`; };
 
 const seeContent = _content => {
-    $(`#sp_title`).innerText = _content.dataset.title;
-    $(`#yt-video`).src = `https://www.youtube.com/embed/${_content.dataset.video}?autoplay=1`;
+    $(`#sp_title`).innerText = _content?.dataset.title || '0';
+    $(`#yt-video`).src = `https://www.youtube.com/embed/${_content?.dataset.video || ''}?autoplay=1`;
+    if ($(`.div-modal`).classList.contains(`hidden`)) openModal();
 }
 
 /** 
@@ -49,4 +53,12 @@ let _loadData = _ => {
                 throw new Error(`HTTP error ${error}`);
             })
     });
+}
+
+const closeModal = ()=> {
+    $(`.div-modal`).classList.add(`hidden`);
+}
+
+const openModal = ()=> {
+    $(`.div-modal`).classList.remove(`hidden`);
 }
