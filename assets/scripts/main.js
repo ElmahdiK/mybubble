d15b67dd84d123504e8499942b483682dae089a8
ElmahdiK <elmahdi.korfed@gmail.com>
2024-12-16 23:43:23 +0100

    feat : skeleton effect added

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,26 +5,25 @@
 //--- for JS selection
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
-const _pathJSON = `./assets/data/mybubble.json`;
+const PATH_DATA = `./assets/data/mybubble.json`;
+
+const article = $(`article`);
+const templateContent = $(`#template-img`);
+for (let i = 0; i < 50; i++) article.append(templateContent.content.cloneNode(true))
 
 window.onload = _ => {
     console.log(`page loaded`);
 
-    let _article = $(`article`);
 
-    _loadData().then(d => {
+    loadData().then(d => {
         let _html = ``;
-        // _html += `<div>`;
         for (let i = 0, j = d.length; i < j; i++) {
-            // _html += `<details open><summary>${d[i].type}</summary><div>`;
             for (let a = 0, b = d[i].content.length; a < b; a++) _html += content(d[i].content[a]);
-            // _html += `</div></details>`;
         }
-        // _html += `</div>`;
-        _article.insertAdjacentHTML(`beforeEnd`, _html);
+        article.innerHTML = _html;
     });
 
-    $(`#bt-modal`).onclick=()=>{
+    $(`#bt-modal`).onclick = () => {
         seeContent(null);
         closeModal();
     }
@@ -37,28 +36,16 @@ const seeContent = _content => {
     if ($(`.div-modal`).classList.contains(`hidden`)) openModal();
 }
 
-/** 
- * @function _loadData
- * @returns {Promise} Promise object
- */
-let _loadData = _ => {
-    return new Promise((resolve, reject) => {
-        fetch(_pathJSON)
-            .then(response => {
-                if (!response.ok) throw new Error(`HTTP error ${response.status}`);
-                return response.json();
-            })
-            .then(_json => resolve(_json))
-            .catch(error => {
-                throw new Error(`HTTP error ${error}`);
-            })
-    });
+const loadData = async _ => {
+    const response = await fetch(PATH_DATA);
+    const data = await response.json();
+    return data;
 }
 
-const closeModal = ()=> {
+const closeModal = () => {
     $(`.div-modal`).classList.add(`hidden`);
 }
 
-const openModal = ()=> {
+const openModal = () => {
     $(`.div-modal`).classList.remove(`hidden`);
 }
