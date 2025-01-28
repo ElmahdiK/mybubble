a86e2b2f4b4a4da4b3c4ddd8c57eb465cdfca7c9
ElmahdiK <elmahdi.korfed@gmail.com>
2025-01-28 19:53:29 +0100

    refactor(main): modal review

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,33 +2,28 @@
  * @author Elmahdi KORFED <elmahdi.korfed@gmail.com>
  */
 
-//--- for JS selection
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
+
 const PATH_DATA = `./assets/data/mybubble.json`;
 
 const article = $(`article`);
+const btnCloseModal = $(`#bt-modal`);
+const btnsNavigation = $$(`#nav-filter button`);
 const templateContent = $(`#template-img`);
 for (let i = 0; i < 50; i++) article.append(templateContent.content.cloneNode(true))
 
 window.onload = _ => {
-    console.log(`page loaded`);
+    loadPictures();
 
-    loadData().then(d => {
-        let _html = ``;
-        for (let i = 0, j = d.length; i < j; i++) _html += content(d[i]);
-        article.innerHTML = _html;
-    });
+    btnCloseModal.onclick = () => closeModal();
 
-    $(`#bt-modal`).onclick = () => {
-        seeContent(null);
-        closeModal();
-    }
-
-    $$(`#nav-filter button`).forEach(_btn => {
+    btnsNavigation.forEach(_btn => {
         _btn.onclick = _ => {
-            $$(`#nav-filter button`).forEach(_b => _b.classList.remove(`active`));
-            _btn.classList.add(`active`);
+            btnsNavigation.forEach(_b => {
+                if (_b.textContent === _btn.innerText) _b.classList.add(`active`);
+                else _b.classList.remove(`active`);
+            });
 
             $$(`article img`).forEach(_img => {
                 if (_btn.innerText === `all`) _img.classList.remove(`hidden`);
@@ -38,12 +33,25 @@ window.onload = _ => {
         }
     })
 }
-const content = _content => { return `<img src="${_content.img}" data-title="${_content.title}" data-type="${_content.type}" data-video="${_content.video}" onclick='seeContent(this)'>`; };
 
-const seeContent = _content => {
+const loadPictures = _ => {
+    loadData().then(d => {
+        let _html = ``;
+        for (let i = 0, j = d.length; i < j; i++) _html += picture(d[i]);
+        article.innerHTML = _html;
+    });
+}
+
+const picture = infosContent => `
+<img src="${infosContent.img}" 
+data-title="${infosContent.title}" 
+data-type="${infosContent.type}" 
+data-video="${infosContent.video}" 
+onclick='setModalContent(this);openModal();'>`;
+
+const setModalContent = _content => {
     $(`#sp_title`).innerText = _content?.dataset.title || '0';
     $(`#yt-video`).src = `https://www.youtube.com/embed/${_content?.dataset.video || ''}?autoplay=1`;
-    if ($(`.div-modal`).classList.contains(`hidden`)) openModal();
 }
 
 const loadData = async _ => {
@@ -53,9 +61,14 @@ const loadData = async _ => {
 }
 
 const closeModal = () => {
-    $(`.div-modal`).classList.add(`hidden`);
+    setModalContent(null);
+    hideModal();
+}
+
+const hideModal = () => {
+    if (!$(`.div-modal`).classList.contains(`hidden`)) $(`.div-modal`).classList.add(`hidden`);
 }
 
 const openModal = () => {
-    $(`.div-modal`).classList.remove(`hidden`);
+    if ($(`.div-modal`).classList.contains(`hidden`)) $(`.div-modal`).classList.remove(`hidden`);
 }
